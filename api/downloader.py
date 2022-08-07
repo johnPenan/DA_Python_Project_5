fcb091ebfd216ebcec1ad4611788f3c7e4e8e464
Jean Penan <jeanpenangoumou1@gmail.com>
2022-08-07 15:14:38 +0200

    Adding Downloader class and docsting update.

--- api/downloader.py
+++ api/downloader.py
@@ -3,6 +3,7 @@ import json
 
 class Downloader:
     """ This class download the open food fact products from API. """
+    
     def __init__(self):
         for i in range(1, 5):
             r = requests.get(f"https://fr.openfoodfacts.org/cgi/search.pl?action=process&json=1&page={i}&page_size=2")
